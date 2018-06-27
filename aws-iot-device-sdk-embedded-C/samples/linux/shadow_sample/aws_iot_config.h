3c1d9500149e6f7fda54809d3468b0b2e367feaa
Bui Trung Kien <trungkiena6@gmail.com>
2018-06-27 21:44:52 +0700

    Update aws iot configuration for shadow

--- aws-iot-device-sdk-embedded-C/samples/linux/shadow_sample/aws_iot_config.h
+++ aws-iot-device-sdk-embedded-C/samples/linux/shadow_sample/aws_iot_config.h
@@ -23,13 +23,13 @@
 
 // Get from console
 // =================================================
-#define AWS_IOT_MQTT_HOST              "" ///< Customer specific MQTT HOST. The same will be used for Thing Shadow
+#define AWS_IOT_MQTT_HOST              "arn:aws:iot:us-east-2:702738637364:thing" ///< Customer specific MQTT HOST. The same will be used for Thing Shadow
 #define AWS_IOT_MQTT_PORT              443 ///< default port for MQTT/S
-#define AWS_IOT_MQTT_CLIENT_ID         "c-sdk-client-id" ///< MQTT client ID should be unique for every device
-#define AWS_IOT_MY_THING_NAME 		   "AWS-IoT-C-SDK" ///< Thing Name of the Shadow this device is associated with
-#define AWS_IOT_ROOT_CA_FILENAME       "rootCA.crt" ///< Root CA file name
-#define AWS_IOT_CERTIFICATE_FILENAME   "cert.pem" ///< device signed certificate file name
-#define AWS_IOT_PRIVATE_KEY_FILENAME   "privkey.pem" ///< Device private key filename
+#define AWS_IOT_MQTT_CLIENT_ID         "devAWSIoT" ///< MQTT client ID should be unique for every device
+#define AWS_IOT_MY_THING_NAME 		   "devAWSIoT" ///< Thing Name of the Shadow this device is associated with
+#define AWS_IOT_ROOT_CA_FILENAME       "certificate.pem.crt" ///< Root CA file name
+#define AWS_IOT_CERTIFICATE_FILENAME   "certificate-CA.pem" ///< device signed certificate file name
+#define AWS_IOT_PRIVATE_KEY_FILENAME   "private.pem.key" ///< Device private key filename
 // =================================================
 
 // MQTT PubSub
